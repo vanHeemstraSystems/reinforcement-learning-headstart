df47bbb59eb23c112ac9d5dbbcd7ae3b597a882b
wvanheemstra <wvanheemstra@users.noreply.github.com>
2022-10-17 14:20:05 +0200

    Create init.js

---
+++ containers/app/rock-paper-scissors/functions/init.js
@@ -0,0 +1,17 @@
+/**
+ *Initialize the model parameters and plot the initial probabilities.
+ */
+
+// Functions
+const { model } = require('./definitions');
+const plotProbs = require('./plotProbs');
+const ts = require('tensorflow');
+
+function init(){
+	//create model class
+	model.add(tf.layers.dense({units: 3, inputShape: [3], useBias: false, kernelInitializer: 'heNormal'}));
+	model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+	plotProbs();
+}
+
+module.exports = { init };

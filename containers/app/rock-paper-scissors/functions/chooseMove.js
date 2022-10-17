efe53ae7d8960ab4140891e2741d50bd36a6d419
wvanheemstra <wvanheemstra@users.noreply.github.com>
2022-10-17 14:30:58 +0200

    Create chooseMove.js

---
+++ containers/app/rock-paper-scissors/functions/chooseMove.js
@@ -0,0 +1,34 @@
+/**
+ * Choose an agent's move based on the phase.
+ * Training Phase: Choose actions randomly
+ * Evaluation Phase: Choose actions based on argmax probability.
+ *
+ * Params: My Move
+ * Return: Agent Move
+ */
+
+
+let myMove;
+let opMove;
+let opMoveIdx;
+
+function chooseMove(move){
+	
+	myMove = move;
+	var phase = document.getElementsByName('phase');
+
+	if(phase[1].checked){
+		//choose from model
+		var intMove = convertToOneHot(myMove);
+		var xs = tf.tensor2d(intMove, [1,3]);
+		var logits = model.predict(xs).arraySync()[0];
+		//get max
+		opMove = moves[getMaxIndex(logits)];
+	}else{
+		//choose randomly 
+	 	opMoveIdx = Math.floor((Math.random() * 3) + 0);
+		opMove = moves[opMoveIdx];
+	}
+
+	return opMove;
+}

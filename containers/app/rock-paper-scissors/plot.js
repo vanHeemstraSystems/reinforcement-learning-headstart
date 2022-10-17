568c37b940de49cd32d4ed976418a68e01bf596f
wvanheemstra <wvanheemstra@users.noreply.github.com>
2022-10-17 13:59:26 +0200

    Create plot.js

---
+++ containers/app/rock-paper-scissors/plot.js
@@ -0,0 +1,27 @@
+function plotProbs(){
+	var divs = ['div1', 'div2', 'div3']
+	var probs;
+	var data;
+	var xs;
+	var logits;
+	for(var i=0;i<3;i++){
+		xs = tf.tensor2d(convertToOneHot(moves[i]), [1, 3]);
+		logits = model.predict(xs).arraySync()[0];
+		probs = tf.softmax(logits).arraySync();
+		data = [
+			{
+				x:moves,
+				y:probs,
+				type:'bar'
+			}
+		];
+
+		var layout = {
+			title: 'What should I play against ' + moves[i] + '?',
+			width: 450,
+			height: 300
+
+		};
+		Plotly.newPlot(divs[i], data, layout);
+	}
+}
